1d4b7912edf3f42f3bdcee4657a8505a3fd6fd07
DaBrookePlayz <73117146+DaBrookePlayz@users.noreply.github.com>
2024-11-26 22:57:25 -0600

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,3 +1,15 @@
-function temp() {
-	alert("test");
+function compParts() {
+	window.location.href = "https://dabrookeplays.github.io/compParts";
+}
+
+function partsQuiz() {
+	window.location.href = "https://dabrookeplays.github.io/partsQuiz";
+}
+
+function buyGuide() {
+	window.location.href = "https://dabrookeplays.github.io/buyGuide";
+}
+
+function contact() {
+	window.location.href = "https://dabrookeplays.github.io/contact";
 }

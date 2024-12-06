c0ee2d5a9ead03e9cdc34744a76aee07b1ccabb9
DaBrookePlayz <73117146+DaBrookePlayz@users.noreply.github.com>
2024-12-06 14:54:46 -0600

    Update partsQuiz.js
    i forgor

--- partsQuiz/partsQuiz.js
+++ partsQuiz/partsQuiz.js
@@ -5,23 +5,23 @@ function welcome() {
 function process() {
 	result = "";
 	if (!document.getElementById("question1answer3").checked) {
-		result = "Question 1 incorrect.\n";
+		result += "Question 1 incorrect.\n";
 	}
 	
 	if (!document.getElementById("question2answer4").checked) {
-		result = "Question 2 incorrect.\n";
+		result += "Question 2 incorrect.\n";
 	}
 	
 	if (!document.getElementById("question3answer1").checked) {
-		result = "Question 3 incorrect.\n";
+		result += "Question 3 incorrect.\n";
 	}
 	
 	if (!document.getElementById("question4answer2").checked) {
-		result = "Question 4 incorrect.\n";
+		result += "Question 4 incorrect.\n";
 	}
 	
 	if (!document.getElementById("question5answer3").checked) {
-		result = "Question 5 incorrect.\n";
+		result += "Question 5 incorrect.\n";
 	}
 	
 	if (result == "") {
